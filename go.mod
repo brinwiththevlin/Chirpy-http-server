4b519d8505b9e824f7b215bf94c4615b94523506
Bryan Cora <corabj@mail.uc.edu>
2025-03-27 19:53:37 -0400

    basic web server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/brinwiththevlin/Chirpy-http-server
+
+go 1.23.6
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	server := http.Server{Handler: mux, Addr: ":8080"}
+
+	server.ListenAndServe()
+}
