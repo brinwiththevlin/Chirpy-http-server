7c5d756871c19ab01f5864d148897ff093e41dcb
Bryan Cora <corabj@mail.uc.edu>
2025-03-29 14:52:56 -0400

    logo and healthz

--- main.go
+++ main.go
@@ -4,8 +4,15 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", healthzHandler)
 	server := http.Server{Handler: mux, Addr: ":8080"}
 
 	server.ListenAndServe()
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte("OK"))
 }
