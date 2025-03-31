4eae99bb68b562403632a079c2586dbec6938321
Bryan Cora <corabj@mail.uc.edu>
2025-03-30 23:07:48 -0400

    middleware

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
 
 func main() {
+	cfg := apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	//fetch files in home directory, removes the prefix /app/
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", healthzHandler)
+	mux.HandleFunc("/metrics", cfg.requestCounts)
+	mux.HandleFunc("/reset", cfg.reset)
 	server := http.Server{Handler: mux, Addr: ":8080"}
 
 	server.ListenAndServe()
@@ -16,3 +24,29 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Write([]byte("OK"))
 }
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Increment the counter
+		cfg.fileserverHits.Add(1)
+
+		// Call the next handler
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) requestCounts(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write(fmt.Appendf(nil, "Hits: %v", cfg.fileserverHits.Load()))
+
+}
+
+func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
+
